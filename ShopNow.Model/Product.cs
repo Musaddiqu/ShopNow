2eb089666d09dcddc5c13c286fc752f3e81b5c07
Musaddiqu <sayyedmusaddique07@gmail.com>
2023-09-27 22:17:48 +0530

    Added Repository and EF common Code

---
+++ ShopNow.Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNow.Model
+{
+    public class Product : IEntity<int>
+    {
+        public int Id { get; set; }
+    }
+}
